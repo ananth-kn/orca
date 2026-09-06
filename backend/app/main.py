f55d28a6c7d7ae8ea6555313cebe50ba66431019
ananth <engr.ananthn@gmail.com>
2026-09-06 17:06:53 +0000

    debug api base url

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,8 +44,8 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= ["https://orca-orpin.vercel.app"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
